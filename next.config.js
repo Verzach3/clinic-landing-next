abce31f7dfaee1e5f665770d3f305a54d9179b91
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-01-10 17:22:32 -0500

    chore: add dockerfile and caprover definition

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
-  }
+    swcPlugins: [['@swc-jotai/react-refresh', {}]]
+  },
+  output: "standalone"
 }
 
 module.exports = nextConfig
