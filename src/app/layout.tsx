a8c5a046f82d1f951a8d4b873b102964814400ad
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-01-25 10:33:48 -0500

    chore: trigger build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
