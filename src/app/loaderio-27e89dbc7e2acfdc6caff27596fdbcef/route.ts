83ef9d7a25f7e60ae26eb85ca045be64d08a5d82
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-02-06 14:07:41 -0500

    fix: loader.io verification

--- src/app/loaderio-27e89dbc7e2acfdc6caff27596fdbcef/route.ts
+++ src/app/loaderio-27e89dbc7e2acfdc6caff27596fdbcef/route.ts
@@ -2,9 +2,11 @@ export async function GET(request: Request): Promise<Response> {
   const str = "loaderio-27e89dbc7e2acfdc6caff27596fdbcef";
   const blob = new Blob([str], { type: "text/plain" });
   console.log(blob);
+  // return new response as a file to download
   return new Response(blob, {
     headers: {
       "content-type": "text/plain",
+      "content-disposition": 'attachment; filename="loaderio-27e89dbc7e2acfdc6caff27596fdbcef"',
     },
   });
 }
