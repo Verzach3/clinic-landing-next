f72666c9b08a556e83a64a8479af086ce7976465
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-02-13 10:12:54 -0500

    update umami script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-      <script async defer src={"https://umami.containers.wellfitclinic.com/script.js"}
+      <script async src={"https://umami.containers.wellfitclinic.com/script.js"}
               data-website-id="ddd1df27-bd8e-472f-85b7-b6d75eb02b11"/>
       <ColorSchemeScript/>
     </head>
